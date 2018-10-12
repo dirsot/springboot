8a5735d171fe9d7d83958ea48a9b0d982f3f17fd
Lukasz Zienkowski <ext-lukasz.zienkowski@finago.com>
2018-10-12 23:43:27 +0200

    2

---
+++ src/test/java/com/example/greatagain/GreatagainApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.greatagain;
+
+import static org.junit.Assert.*;
+
+/**
+ * This class
+ *
+ * @author ext-lukasz.zienkowski
+ * @since 12.10.2018
+ */
+public class GreatagainApplicationTest {
+
+}
